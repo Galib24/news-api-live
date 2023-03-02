b88961e48184947b718b4fd3e8df0f261c06fa12
SQLAdmin <abuyeahia24@gmail.com>
2023-03-02 14:35:06 +0600

    added modal with news details there

--- js/news.js
+++ js/news.js
@@ -42,7 +42,7 @@ const newsContainer = document.getElementById('all-news');
 newsContainer.innerHTML = '';
 
 data.forEach(singleNews => {
-    console.log(singleNews)
+    const {_id,image_url,title,details,author,total_view} = (singleNews)
     // newsContainer.innerHTML +=
    const  card = document.createElement('div');
    card.classList.add('card', 'mb-3');
@@ -50,13 +50,33 @@ data.forEach(singleNews => {
    
    <div class="row g-0">
    <div class="col-md-4">
-     <img src="${singleNews.image_url}" class="img-fluid rounded-start" alt="...">
+     <img src="${image_url}" class="img-fluid rounded-start" alt="...">
    </div>
-   <div class="col-md-8">
+   <div class="col-md-8 d-flex flex-column">
      <div class="card-body">
-       <h5 class="card-title">${singleNews.title}</h5>
-       <p class="card-text">${singleNews.details.slice(0, 200)}</p>
-       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+       <h5 class="card-title">${title}</h5>
+       <p class="card-text">${details.slice(0, 200)}</p>
+     </div>
+     <div class="card-footer border-0 bg-body d-flex justify-content-between">
+     <div class= "d-flex gap-2">
+     <img src="${author.img}" class="img-fluid rounded-circle" alt="..." height="40" width="40">
+     <div>
+     <p class = "m-0 p-0"> ${singleNews.author.name}</p>
+     <p class = "m-0 p-0"> ${singleNews.author.published_date}</p>
+     </div>
+     
+     </div>
+     <div class = "d-flex align-items-center">
+     <i class="fa-solid fa-eye"></i>
+     <p class = "m-0 p-0"> ${total_view}</p>
+     <i class="fa-solid fa-star"></i>
+     
+     </div>
+     <div>
+     <i class="fa-solid fa-arrow-right" onclick="fetchNewDetails('${_id}')" data-bs-toggle="modal"
+      data-bs-target="#exampleModal"></i>
+     
+     </div>
      </div>
    </div>
  </div>
@@ -64,5 +84,58 @@ data.forEach(singleNews => {
    `;
    newsContainer.appendChild(card)
 
-})
+});
 };
+
+// show News Details
+const fetchNewDetails = news_id=>{
+  let url = `https://openapi.programming-hero.com/api/news/${news_id}`
+// console.log(url);
+fetch(url)
+.then(res => res.json())
+.then(data => showNewDetail(data.data[0]))
+} 
+const showNewDetail = (newsDetail) =>{
+// 
+const {_id,image_url,title,details,author,total_view} = newsDetail;
+
+document.getElementById("modal-body").innerHTML = `
+   
+   <div class="card mb-3">
+   <div class="row g-0">
+   <div class="col-md-12">
+     <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+   </div>
+   <div class="col-md-12 d-flex flex-column">
+     <div class="card-body">
+       <h5 class="card-title">${title}</h5>
+       <p class="card-text">${details}</p>
+     </div>
+     <div class="card-footer border-0 bg-body d-flex justify-content-between">
+     <div class= "d-flex gap-2">
+     <img src="${author.img}" class="img-fluid rounded-circle" alt="..." height="40" width="40">
+     <div>
+     <p class = "m-0 p-0"> ${author.name}</p>
+     <p class = "m-0 p-0"> ${author.published_date}</p>
+     </div>
+     
+     </div>
+     <div class = "d-flex align-items-center">
+     <i class="fa-solid fa-eye"></i>
+
+     <p class = "m-0 p-0"> ${total_view}</p>
+     </div>
+
+     <div>
+     <i class="fa-solid fa-star"></i>
+     </div>
+     
+     </div>
+   </div>
+ </div>
+   </div>
+   
+   
+   `;
+   
+}
